55a180c412995d0d1341f2ac20db31e049ea315a
Ahmad Houhou <ahmadhouhou@device-56.home>
2025-11-06 21:30:43 +0100

    authy

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://woquxjugakbrprlfaxqj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvcXV4anVnYWticnBybGZheHFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTQwMTksImV4cCI6MjA3ODAzMDAxOX0._OBFUgg6wqSkkwiapnYsT9XiFUpzb-XEgyKCCWH1WOA"
+);
